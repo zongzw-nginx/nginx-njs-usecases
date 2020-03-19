0cdcf9ad64de5a21026ba632c5ac24ff3b4fc144
Andrew Zong <a.zong@f5.com>
2020-03-19 21:43:52 +0800

    Regulate the folders for convenience of adding other use cases.

---
+++ autoindex_prev/data/README.txt
@@ -0,0 +1,5 @@
+The RX simulator offers two rx-specific configure options:
+
+--enable-cycle-accurate  (default)
+--disable-cycle-accurate
+
--- compose.sh
+++ compose.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 export HOMEDIR=`cd $(dirname $0); pwd`
+export AUTOINDEX_PREV=$HOMEDIR/autoindex_prev
 
 docker-compose -f $HOMEDIR/docker-compose.yml up -d --force-recreate
