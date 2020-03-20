98488937b0ecd57f59ce663cfd53981863b9bd57
Andrew Zong <a.zong@f5.com>
2020-03-20 20:30:18 +0800

    Add fast response use case( to be renew.)

--- compose.sh
+++ compose.sh
@@ -4,5 +4,6 @@ export HOMEDIR=`cd $(dirname $0); pwd`
 export AUTOINDEX_PREV=$HOMEDIR/autoindex_prev
 export DATA_MASKING=$HOMEDIR/data_masking
 export VIRTUAL_PATCHING=$HOMEDIR/virtual_patching
+export FAST_RESPONSE=$HOMEDIR/fast_response
 
 docker-compose -f $HOMEDIR/docker-compose.yml up -d --force-recreate
---
+++ fast_response/response.js
@@ -0,0 +1,21 @@
+function response(r) {
+    var n = 0;
+    function done(res) {
+
+        r.log(`res.parent: ${res.parent.uri}, res.uri: ${res.uri}`);
+        if (++n == 1) {
+            r.return(200, res.responseBody);
+        }
+    }
+
+    r.subrequest('/r1', done);
+    r.subrequest('/r2', done);
+}
+
+function response1(r) {
+    r.return(200, 'response 1');
+}
+
+function response2(r) {
+    r.return(200, 'response 2');
+}
